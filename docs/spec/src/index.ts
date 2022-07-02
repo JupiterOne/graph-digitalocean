3da74da10d67443283f289d6ada15457f6c210b0
Matthew Zember <matthew.zember@jupiterone.com>
2022-07-02 06:04:49 -0400

    fix typo

--- docs/spec/src/index.ts
+++ docs/spec/src/index.ts
@@ -2,7 +2,7 @@ import { IntegrationSpecConfig } from '@jupiterone/integration-sdk-core';
 
 import { IntegrationConfig } from '../../../src/config';
 import { accountSpec } from './account';
-import { alertPolicySpec } from './alertPolciy';
+import { alertPolicySpec } from './alertPolicy';
 import { databaseSpec } from './databases';
 import { domainSpec } from './domains';
 import { dropletsSpec } from './droplets';
